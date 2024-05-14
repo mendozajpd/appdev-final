0a2578b6ab7b003a570172ecf5e41483a01d3f00
mendozajpd <98361827+mendozajpd@users.noreply.github.com>
2024-05-14 09:02:14 +0800

    added controllers

--- laravel/app/Http/Controllers/MatchController.php
+++ laravel/app/Http/Controllers/MatchController.php
@@ -8,5 +8,76 @@ use Illuminate\Http\Request;
 
 class MatchController extends Controller
 {
-    //
+    public function createMatch(Request $request) {
+        $matchRequest = new MatchRequest();
+        $matchRequest->user_1 = $request->user_1;
+        $matchRequest->user_2 = $request->user_2;
+        $matchRequest->pet_id = $request->pet_id;
+        $matchRequest->is_accepted = false;
+        $matchRequest->is_matched = false;
+        $matchRequest->save();
+        
+        return redirect()->route('match.index');
+    }
+    
+    public function acceptMatch(Request $request) {
+        $matchRequest = MatchRequest::find($request->id);
+        $matchRequest->is_accepted = true;
+        $matchRequest->save();
+        
+        $matched = new Matched();
+        $matched->match_id = $matchRequest->id;
+        $matched->user_id = $matchRequest->user_1;
+        $matched->pet_id = $matchRequest->pet_id;
+        $matched->save();
+        
+        $matched = new Matched();
+        $matched->match_id = $matchRequest->id;
+        $matched->user_id = $matchRequest->user_2;
+        $matched->pet_id = $matchRequest->pet_id;
+        $matched->save();
+        
+        return redirect()->route('match.index');
+    }
+    
+    public function rejectMatch(Request $request) {
+        $matchRequest = MatchRequest::find($request->id);
+        $matchRequest->is_accepted = false;
+        $matchRequest->save();
+        
+        return redirect()->route('match.index');
+    }
+
+    public function destroyMatch(Request $request) {
+        $matchRequest = MatchRequest::find($request->id);
+        $matchRequest->delete();
+        
+        return redirect()->route('match.index');
+    }
+
+    public function destroyMatched(Request $request) {
+        $matched = Matched::find($request->id);
+        $matched->delete();
+        
+        return redirect()->route('match.index');
+    }
+
+    public function destroyAllMatched() {
+        Matched::truncate();
+        
+        return redirect()->route('match.index');
+    }
+
+    public function destroyAllMatchRequest() {
+        MatchRequest::truncate();
+        
+        return redirect()->route('match.index');
+    }
+
+    public function destroyAll() {
+        Matched::truncate();
+        MatchRequest::truncate();
+        
+        return redirect()->route('match.index');
+    }
 }
--- laravel/app/Http/Controllers/PetController.php
+++ laravel/app/Http/Controllers/PetController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserPet;
+use App\Models\PetInfo;
 use Illuminate\Http\Request;
 
 class PetController extends Controller
